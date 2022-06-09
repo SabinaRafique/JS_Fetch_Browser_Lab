e9bdf8b289c6cb76f63b4e369bc04d3d4763f9c2
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-06-10 00:07:42 +0100

    getCountryByName() refactored to return country name and population

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const getCountryByName = (countryName) => {
     fetch(`https://restcountries.com/v2/name/${countryName}`)
     .then(response => response.json())
-    .then(data => document.querySelector("p").innerText = "Country: " + data[0].name)
+    .then(data => document.querySelector("p").innerText = "Country: " + data[0].name + ", Population: " + data[0].population)
 
     const sect = document.querySelector("#sect")
     const setData = document.createElement("p")
